0a3ffbd6579ba4b2b0b874be9c195d717bf6de02
Joseph Hastings <jwhastings21@gmail.com>
2022-12-06 07:52:24 -0500

    add week 13, check to see if updated folder structure works

--- docs/_render_rmd.R
+++ docs/_render_rmd.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 s  <- 2022 # current season
-wk <- 12   # current week
+wk <- 13   # current week
 
 # Render index
 rmarkdown::render(
@@ -19,12 +19,12 @@ rmarkdown::render(
 )
 
 # Render current weekly report
-# rm(list = setdiff(ls(), c("s", "wk")))
-# rmarkdown::render(
-#   "weekly_report.Rmd",
-#   params = list(current_week = wk, current_season = s),
-#   output_file = paste0("weekly_reports/", s, "/week", wk)
-# )
+rm(list = setdiff(ls(), c("s", "wk")))
+rmarkdown::render(
+  "weekly_report.Rmd",
+  params = list(current_week = wk, current_season = s),
+  output_file = paste0("weekly_reports/", s, "/week", wk, "/index.html")
+)
 
 # Render all reports to from week 1 to current week
 for (i in 1:wk) {
