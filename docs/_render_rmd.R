c5b3e2115ebd0ddacb7aa98f3ba8dcbea6c5db5f
Joseph Hastings <jwhastings21@gmail.com>
2022-12-06 08:13:49 -0500

    update folder structure, all pages are index.html, fixed links

--- docs/_render_rmd.R
+++ docs/_render_rmd.R
@@ -15,24 +15,31 @@ rm(list = setdiff(ls(), c("s", "wk")))
 rmarkdown::render(
   "league_history.Rmd",
   params = list(current_week = wk, current_season = s),
-  output_file = "league_history/league_history"
+  output_file = "league_history/index"
 )
 
 # Render current weekly report
-rm(list = setdiff(ls(), c("s", "wk")))
-rmarkdown::render(
-  "weekly_report.Rmd",
-  params = list(current_week = wk, current_season = s),
-  output_file = paste0("weekly_reports/", s, "/week", wk, "/index.html")
-)
+# rm(list = setdiff(ls(), c("s", "wk")))
+# 
+# out_dir <- paste0("weekly_reports/", s, "/week", wk)
+# if (!dir.exists(out_dir)) dir.create(out_dir)
+# 
+# rmarkdown::render(
+#   "weekly_report.Rmd",
+#   params = list(current_week = wk, current_season = s),
+#   output_file = paste0(out_dir, "/index")
+# )
 
 # Render all reports to from week 1 to current week
 for (i in 1:wk) {
   rm(list = setdiff(ls(), c("s", "wk", "i")))
+  
+  out_dir <- paste0("weekly_reports/", s, "/week", i)
+  if (!dir.exists(out_dir)) dir.create(out_dir)
 
   rmarkdown::render(
     "weekly_report.Rmd",
     params = list(current_week = i, current_season = s),
-    output_file = paste0("weekly_reports/", s, "/week", i)
+    output_file = paste0(out_dir, "/index")
   )
 }
